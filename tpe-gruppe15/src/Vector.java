ada8125d4c6b87f9db5d31ba3eb8f113ab10cd64
1428038 <1428038@PC26.rz.hs-mannheim.de>
2015-03-12 14:00:55 +0100

    initial commit

---
+++ tpe-gruppe15/src/Vector.java
@@ -0,0 +1,13 @@
+
+public class Vector {
+
+	public Vector(int i, int j, int k) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public double betrag() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
