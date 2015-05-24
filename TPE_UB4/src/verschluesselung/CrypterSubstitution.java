60ca4b6a69b5ee72e3ac5fb0bf36c1436f856c9b
bsra <buesra_yildiz@live.de>
2015-05-24 14:50:02 +0200

    Subsitutionschiffre: Angefangen

--- TPE_UB4/src/verschluesselung/CrypterSubstitution.java
+++ TPE_UB4/src/verschluesselung/CrypterSubstitution.java
@@ -1,5 +1,36 @@
 package verschluesselung;
 
 public class CrypterSubstitution implements Crypter {
-	//Methode für verschlüsselung
+	// Methode für verschlüsselung
+
+	private String klartext = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private String verschlusselt = "UFLPWDRASJMCONQYBVTEXHZKGI";
+
+	@Override
+	public String verschluesseln(String key, String message)
+	// throws IllegalKeyException, IllegalMessageException
+	{
+		String geheimeBotschaft= " ";
+		for (int i = 0; i < message.length(); i++) {
+			for (int j = 0; j < klartext.length(); j++) {
+				if(message.charAt(i)==klartext.charAt(j)){
+					i=j;
+					char c=' ';
+					c=verschlusselt.charAt(i);
+					System.out.println(c);
+					//ich komme nicht mehr weiter :D
+				}
+				
+			}
+			
+		}
+		return geheimeBotschaft;
+		
+	}
+@Override
+public String entschluesseln(String key, String cypherText)
+		throws IllegalKeyException, IllegalMessageException {
+	// TODO Auto-generated method stub
+	return null;
+}
 }
--- TPE_UB4/src/verschluesselung/Main.java
+++ TPE_UB4/src/verschluesselung/Main.java
@@ -5,13 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		//Aufgabe: Kombination der Schlüssel verwenden
 		
-		CrypterCaesar jap = new CrypterCaesar();
+//		CrypterCaesar jap = new CrypterCaesar();
+//		
+//		String ergebnis = jap.verschluesseln("B", "HALLO");
+//		System.out.println(ergebnis);
+//		
+//		String rueck = jap.entschluesseln("B", "JCNNQ");
+//		System.out.println(rueck);
 		
-		String ergebnis = jap.verschluesseln("B", "HALLO");
+		CrypterSubstitution hey = new CrypterSubstitution();
+		String ergebnis = hey.verschluesseln(null, "B");
 		System.out.println(ergebnis);
-		
-		String rueck = jap.entschluesseln("B", "JCNNQ");
-		System.out.println(rueck);
-	}
-
+	
+}
 }
