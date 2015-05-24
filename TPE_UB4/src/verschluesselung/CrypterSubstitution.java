50a83dc7d7279c957c84578ecc0d4e03b96db23f
Ilker <ilker.turan@outlook.com>
2015-05-24 18:09:46 +0200

    Substitutionsverschlüsselung

--- TPE_UB4/src/verschluesselung/CrypterSubstitution.java
+++ TPE_UB4/src/verschluesselung/CrypterSubstitution.java
@@ -1,7 +1,7 @@
 package verschluesselung;
 
 public class CrypterSubstitution implements Crypter {
-	// Methode fÃ¼r verschlÃ¼sselung
+	// Methode für verschlüsselung
 
 	private String klartext = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	private String verschlusselt = "UFLPWDRASJMCONQYBVTEXHZKGI";
@@ -10,27 +10,27 @@ public class CrypterSubstitution implements Crypter {
 	public String verschluesseln(String key, String message)
 	// throws IllegalKeyException, IllegalMessageException
 	{
-		String geheimeBotschaft= " ";
+		String geheimeBotschaft = " ";
+		String endergebnis = "";
 		for (int i = 0; i < message.length(); i++) {
 			for (int j = 0; j < klartext.length(); j++) {
-				if(message.charAt(i)==klartext.charAt(j)){
-					i=j;
-					char c=' ';
-					c=verschlusselt.charAt(i);
-					System.out.println(c);
-					//hier gehts dann weiter :D
+				if (message.charAt(i) == klartext.charAt(j)) {
+					geheimeBotschaft = verschlusselt.substring(j, j + 1);
+					endergebnis = endergebnis + geheimeBotschaft;
 				}
-				
+
 			}
-			
+
 		}
-		return geheimeBotschaft;
-		
+
+		return endergebnis;
+
 	}
-@Override
-public String entschluesseln(String key, String cypherText)
-		throws IllegalKeyException, IllegalMessageException {
-	// TODO Auto-generated method stub
-	return null;
-}
+
+	@Override
+	public String entschluesseln(String key, String cypherText)
+			throws IllegalKeyException, IllegalMessageException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- TPE_UB4/src/verschluesselung/Main.java
+++ TPE_UB4/src/verschluesselung/Main.java
@@ -5,17 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 		//Aufgabe: Kombination der Schlüssel verwenden
 		
-//		CrypterCaesar jap = new CrypterCaesar();
-//		
-//		String ergebnis = jap.verschluesseln("B", "HALLO");
-//		System.out.println(ergebnis);
-//		
-//		String rueck = jap.entschluesseln("B", "JCNNQ");
-//		System.out.println(rueck);
+		CrypterCaesar jap = new CrypterCaesar();
+		
+		String ergebnis = jap.verschluesseln("B", "HALLO");
+		System.out.println(ergebnis);
+		
+		String rueck = jap.entschluesseln("B", "JCNNQ");
+		System.out.println(rueck);
 		
 		CrypterSubstitution hey = new CrypterSubstitution();
-		String ergebnis = hey.verschluesseln(null, "B");
-		System.out.println(ergebnis);
+		String ergebnis1 = hey.verschluesseln(null, "BALU");
+		System.out.println(ergebnis1);
+		
+	}
 	
-}
+
 }
